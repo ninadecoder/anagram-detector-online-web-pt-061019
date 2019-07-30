dc9a904a9779eb3d95843b5f1b6ad91165837faa
Nina Kazzim <kazzim@icloud.com>
2019-07-30 14:26:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,6 +6,6 @@ class Anagrams
   end
   
   def match(arr)
-    arr.each { |some_word| some_word.split("").sort ==  some_word.split("").sort }
+    arr.select { |some_word| some_word.split("").sort ==  some_word.split("").sort }
   end
 end
